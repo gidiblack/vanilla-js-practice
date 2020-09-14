fb5da44910a5395f0537d31083a28c03439f9066
Tha Gidi <you@example.com>
2020-09-14 23:36:19 +0100

    navbar done

--- app.js
+++ app.js
@@ -1,95 +1,14 @@
 'use strict';
-// local reviews data
-const reviews = [
-  {
-    id: 1,
-    name: "susan smith",
-    job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-  },
-  {
-    id: 3,
-    name: "peter jones",
-    job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
-  {
-    id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  },
-];
-// select items from the DOM
-const img = document.getElementById("person-img");
-const author = document.getElementById("author");
-const job = document.getElementById("job");
-const info = document.getElementById("info");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const randomBtn = document.querySelector(".random-btn");
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
 
-// set starting item
-let currentItem = 0;
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
 
-// load initial item
-window.addEventListener('DOMContentLoaded', function (){
-    // access and show first item onLoad
-    showPerson(currentItem);
-});
-
-// show person based on item...person parsed as a parameter
-function showPerson(person){
-    const item = reviews[person];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-}
-
-// show next person
-nextBtn.addEventListener('click', function (){
-    // increment currentItem
-    currentItem++;
-    //check if currentItem is greater than array length-1 then set currentItem to 0 to start over
-    if(currentItem > reviews.length - 1){
-        currentItem = 0;
-    }
-    showPerson(currentItem);
-});
-
-// show previous person
-prevBtn.addEventListener('click', function (){
-    // decrement currentItem
-    currentItem--;
-    // check if currentItem is less than 0 then set currentItem to array length-1 to start from the last item in array
-    if(currentItem < 0){
-        currentItem = reviews.length - 1;
-    }
-    showPerson(currentItem);
-});
-
-// show random person
-randomBtn.addEventListener('click', function(){
-    // get random number by multiplying the length of the array with the result of math.random which is always between 0 - 1, never 1...then wrap with math.floor to round down to the nearest interger (e.g 1.56767 becomes 1)
-    currentItem = Math.floor(Math.random() * reviews.length);
-    showPerson(currentItem);
+navToggle.addEventListener('click', function (){
+    // toggle show-links class on links element onClick
+    links.classList.toggle('show-links');
 });
