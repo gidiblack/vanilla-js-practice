7426d828ccce61403b02dcd50debf108be39aa2f
Tha Gidi <you@example.com>
2020-09-14 23:15:00 +0100

    reviews done

--- app.js
+++ app.js
@@ -1,35 +1,95 @@
 'use strict';
-// set initial count to 0
-let count = 0;
-const value = document.querySelector("#value");
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+// select items from the DOM
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
 
-// select all buttons to use forEach method on the nodelist returned (nodelist is array-like)
-const btns = document.querySelectorAll(".btn");
+// set starting item
+let currentItem = 0;
 
-//parameter parsed into forEach callback function reps each item/element in the 'btns' nodelist
-btns.forEach(function (btn) {
-    // e parameter represents event object to target which btn was clicked
-    btn.addEventListener('click', function (e){
-        const styles =  e.currentTarget.classList;
-        // conditional statements to check if currentTarget containes certain classes
-        if (styles.contains('decrease')){
-            count--;
-        } else if (styles.contains('increase')){
-            count++;
-        } else if (styles.contains('reset')){
-            count = 0;
-        }
-        // check if count is greater than, less than or equal to 0 then change value color
-        if (count > 0){
-            value.style.color = 'green';
-        }
-        if (count < 0){
-            value.style.color = 'red';
-        }
-        if (count === 0){
-            value.style.color = "black";
-        }
-        value.textContent = count;
-        console.log(value);
-    });
+// load initial item
+window.addEventListener('DOMContentLoaded', function (){
+    // access and show first item onLoad
+    showPerson(currentItem);
+});
+
+// show person based on item...person parsed as a parameter
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function (){
+    // increment currentItem
+    currentItem++;
+    //check if currentItem is greater than array length-1 then set currentItem to 0 to start over
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+// show previous person
+prevBtn.addEventListener('click', function (){
+    // decrement currentItem
+    currentItem--;
+    // check if currentItem is less than 0 then set currentItem to array length-1 to start from the last item in array
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener('click', function(){
+    // get random number by multiplying the length of the array with the result of math.random which is always between 0 - 1, never 1...then wrap with math.floor to round down to the nearest interger (e.g 1.56767 becomes 1)
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
 });
