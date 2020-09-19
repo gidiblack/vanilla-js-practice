dce0b815f097cbad9b72b5b2905489dc2161a3b2
Tha Gidi <you@example.com>
2020-09-19 14:38:30 +0100

    navbar scrolling done

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
 // ********** set date ************
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
@@ -31,7 +27,8 @@ const topLink = document.querySelector('.top-link');
 // scroll event listener on window element
 window.addEventListener('scroll', function(){
     // get vertical scroll height
-    const scrollHeight = window.pageYOffset;
+    // pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+    const scrollHeight = window.pageYOffset; 
     // get navbar height
     const navHeight = navbar.getBoundingClientRect().height;
     // check if vertical scroll height is greater than navbar height
@@ -53,3 +50,38 @@ window.addEventListener('scroll', function(){
 });
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+// add click event listener for Each link
+scrollLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+        // prevent default scrolling
+        e.preventDefault();
+        // slice the first character[0] off the attribute string -- slice extracts a section of a string without modifying original string
+        const id = e.currentTarget.getAttribute('href').slice(1); 
+        // console.log(id);
+        const element = document.getElementById(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+        //offsetTop - A Number, representing the top position of the element, in pixels
+        let position = element.offsetTop - navHeight;
+        // console.log(position);
+        // check if fixedNav returns false
+        if (!fixedNav){
+            // subtract navHeight from position
+            position = position - navHeight;
+        }
+        // check if navHeight is greather than 85 (ie on mobile screens)
+        if (navHeight > 85){
+            // add containerHeight to position
+            position = position + containerHeight;
+        }
+        // navigate to specific sections
+        window.scrollTo({
+            left: 0,
+            top: position
+        });
+        // remove container height on small screens when link is clicked
+        linksContainer.style.height = 0;
+    });
+});
