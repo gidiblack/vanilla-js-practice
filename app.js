91b30ce0b9c242df96d6db95baf5e38b089eca08
Gideon Etim <gidiblack@gmail.com>
2021-01-08 02:54:55 +0100

    recipe app done

--- app.js
+++ app.js
@@ -1,159 +1,220 @@
-const quizData = [
-    {
-        question: "What is the most used programming language in 2019?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "JavaScript",
-        correct: "d",
-    },
-    {
-        question: "Who is the President of US?",
-        a: "Florin Pop",
-        b: "Donald Trump",
-        c: "Ivan Saldano",
-        d: "Mihai Andrei",
-        correct: "b",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Cascading Style Sheet",
-        c: "Jason Object Notation",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
-    {
-        question: "When did Nigerian become a Republic?",
-        a: "1996",
-        b: "1963",
-        c: "1960",
-        d: "none of the above",
-        correct: "b",
-    },
-    {
-        question: "What is the capital of Kenya?",
-        a: "Nairobi",
-        b: "Tokyo",
-        c: "Lagos",
-        d: "Kigali",
-        correct: "a",
-    },
-    {
-        question: "Who was the President of the US in 2010?",
-        a: "Donald Trump",
-        b: "George Bush",
-        c: "Barrack Obama",
-        d: "Bill CLinton",
-        correct: "c",
-    },
-    {
-        question: "What does DVD stand for?",
-        a: "Digital Version Database",
-        b: "Diagonal Visible Divide",
-        c: "Digital Versatile Disk",
-        d: "none of the above",
-        correct: "c",
-    },
-    {
-        question: "Which of these is NOT a programming language?",
-        a: "Python",
-        b: "Ruby",
-        c: "C+",
-        d: "Bootstrap",
-        correct: "d",
-    },
-    {
-        question: "Which of these is an output device?",
-        a: "LED Screen",
-        b: "Keyboard",
-        c: "Touchpad",
-        d: "Scanner",
-        correct: "a",
-    }
-];
-// declare element variables
-const quiz = document.getElementById('quiz');
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
-const answers = document.querySelectorAll('.answer');
-// set quiz and score counters to 0
-let currentQuiz = 0;
-let score = 0;
+const mealsEl = document.getElementById("meals");
+const favoriteContainer = document.getElementById("fav-meals");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById("close-popup");
 
-loadQuiz();
-// function to load quiz data when the page loads 
-function loadQuiz(){
-    // invoke function to deselect answers
-    deselectAnswers();
-    // get first quiz data by declaring const
-    const currentQuizData = quizData[currentQuiz]; 
-    // set inner text of question element 
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
+
+getRandomMeal();
+fetchFavMeals();
+
+async function getRandomMeal(){
+    // async function to fetch data from API
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    // save response in json object
+    const respData = await resp.json();
+    // assign first recipe to randomMeal variable
+    const randomMeal = respData.meals[0];
+    // use randomMeal as a param to addMeal to the screen
+    addMeal(randomMeal, true);
 }
 
-// Get value of selected answer
-function getSelected(){
-    // set answer to undefined so as to return it when the function executes
-    let answer = undefined;
-    // loop thru radio buttons and thier values
-    answerEls.forEach((answerEl) => {
-        // check if radio btn is selected
-        if (answerEl.checked) {
-            // set answer to id of selected radio element
-            answer = answerEl.id;
-        }
-    });
-    return answer;
-}
-// function to deselect radio buttons when new quiz loads
-function deselectAnswers() {
-    answerEls.forEach((answerEl) => {
-        answerEl.checked = false;
-    });
+async function getMealById(id) {
+    const resp = await fetch( "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+
+    const respData = await resp.json();
+
+    const meal = respData.meals[0];
+
+    return meal;
 }
 
-// click event listener on submit btn
-submitBtn.addEventListener('click', () => {
-    // store answer returned from getSelected function in 'answer' const
-    const answer = getSelected();
+
+async function getMealsBySearch(term){
+    const resp = await fetch( "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term );
+
+    const respData = await resp.json();
+
+    const meals = respData.meals;
+
+    return meals;
+}
+
+// add meal to screen
+function addMeal(mealData, random = false ){
     
-    if (answer) {
-        // check if answer selected is correct
-        if (answer === quizData[currentQuiz].correct) {
-            // increment score counter by 1
-            score++;
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML = `
+    <div class="meal-header">
+        ${ random ? `<span class="random"> Random Recipe </span>` : "" }
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+    </div>
+    <div class="meal-body">
+        <h4>${mealData.strMeal}</h4>
+        <button class="fav-btn">
+            <i class="fas fa-heart"></i>
+        </button>
+    </div>
+`;
+    // select fav-btn and add Event listener to toggle active class on click 
+    const btn = meal.querySelector(".meal-body .fav-btn");
+    
+    btn.addEventListener("click", () => {
+        // check if btn is active then remove class and run remove from LS function
+        if (btn.classList.contains("active")){
+            removeMealLS(mealData.idMeal);
+            btn.classList.remove('active');
+        } 
+        // else add active class and run add to LS function
+        else {
+            addMealLS(mealData.idMeal);
+            btn.classList.add('active');
         }
-        // increment quiz counter by 1
-        currentQuiz++;
-        // check if quiz counter is less than total quiz length
-        if (currentQuiz < quizData.length) {
-            // load new quiz
-            loadQuiz();
+        fetchFavMeals();
+    });
+    // show recipe on click
+    meal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
+    // append new meals to screen
+    mealsEl.appendChild(meal);
+}
+// add meal to local storage
+function addMealLS(mealId) {
+    // first get meals from LS if any
+    const mealIds = getMealsLS();
+    // add new meal to LS array
+    localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+}
+
+// remove meal from Local storage
+function removeMealLS(mealId) {
+    // first get meals from LS if any
+    const mealIds = getMealsLS();
+    // filter ids that are NOT the mealId parsed to remove the parsed meal
+    localStorage.setItem( "mealIds", JSON.stringify(mealIds.filter(id => id !== mealId)));
+}
+
+// get meals data stored in Local storage
+function getMealsLS(){
+    const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+    // return mealIds if any else return empty array
+    return mealIds === null ? [] : mealIds;
+}
+
+async function fetchFavMeals() {
+    // clean container
+    favoriteContainer.innerHTML = "";
+
+    // get mealIds by invoking func
+    const mealIds = getMealsLS();
+
+    // loop thru each id then addFavMeal
+    for(let i=0; i < mealIds.length; i++) {
+        const mealId = mealIds[i];
+        meal =  await getMealById(mealId);
+        // meals.push(meal);
+
+        addFavMeal(meal);
+    }
+}
+
+function addFavMeal(mealData){
+    const favMeal = document.createElement("li");
+
+    favMeal.innerHTML = `
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        /><span>${mealData.strMeal}</span>
+        <button class="clear"><i class="fas fa-window-close"></i></button>
+    `;
+    // select clear button
+    const btn = favMeal.querySelector(".clear");
+    // add listener to remove meal from ls
+    btn.addEventListener("click", () => {
+        removeMealLS(mealData.idMeal);
+        // invoke fetch fav meals func
+        fetchFavMeals();
+    });
+
+    favMeal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
+    favoriteContainer.appendChild(favMeal);
+}
+
+function showMealInfo(mealData) {
+    // clean it up
+    mealInfoEl.innerHTML = "";
+
+    // update the Meal info
+    const mealEl = document.createElement("div");
+
+    const ingredients = [];
+
+    // get ingredients and measures
+    for (let i = 1; i <= 20; i++) {
+        if (mealData["strIngredient" + i]) {
+            ingredients.push(
+                `${mealData["strIngredient" + i]} - ${
+                    mealData["strMeasure" + i]
+                }`
+            );
         } else {
-            // else display this message
-            quiz.innerHTML = `
-                <h2>You answered ${score}/${quizData.length} questions correctly.</h2>
-                
-                <button onclick="location.reload()">Reload</button>
-            `;
+            break;
         }
     }
+
+    mealEl.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients.map(
+                (ing) => `<li>${ing}</li>`).join("")
+            }
+        </ul>
+    `;
+
+    mealInfoEl.appendChild(mealEl);
+
+    // show the popup
+    mealPopup.classList.remove("hidden");
+}
+
+// search for meals
+searchBtn.addEventListener("click", async () => {
+    // clean container
+    mealsEl.innerHTML = "";
+
+    const search = searchTerm.value;
+    // save search results in meals
+    const meals = await getMealsBySearch(search);
+    // check if meals is NOT empty 
+    if (meals) {
+        meals.forEach((meal) => {
+            // add each meal to screen
+            addMeal(meal);
+        });
+    }
+});
+
+popupCloseBtn.addEventListener("click", () => {
+    mealPopup.classList.add("hidden");
 });
